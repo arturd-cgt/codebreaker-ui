4811838986d7391f72c4abc9257cd88d4544b82f
Artur Drabinka <artur.d@cgt.group>
2025-11-18 12:41:15 +0100

    infra: add react query and axios for networking

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { NavigationContainer } from '@/navigation';
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { NavigationContainer } from "@/navigation";
+
+// Create a query client
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <NavigationContainer />
+    </QueryClientProvider>
   );
 }
